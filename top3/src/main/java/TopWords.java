3a949d063ee9c9a45377cccec0b2c7a1cd454c7e
droidsam1 <droidsam@gmail.com>
2023-01-15 11:49:50 +0100

    refactor: add semantics to regex

--- top3/src/main/java/TopWords.java
+++ top3/src/main/java/TopWords.java
@@ -5,16 +5,19 @@ import static java.util.stream.Collectors.toMap;
 
 public class TopWords {
 
+    public static final String ANY_SPECIAL_CHAR = "[!\\W]";
+
     public static List<String> top3(String s) {
         if (s.isBlank()) {
             return Collections.emptyList();
         }
-        return Arrays.stream(s.split("[!\\W]")).map(String::toLowerCase)
+        return Arrays.stream(s.split(ANY_SPECIAL_CHAR)).map(String::toLowerCase)
                 .map(word -> new AbstractMap.SimpleEntry<>(word, 1))
                 .collect(toMap(AbstractMap.SimpleEntry::getKey, AbstractMap.SimpleEntry::getValue, Integer::sum))
                 .entrySet().stream()
                 .sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
                 .map(Map.Entry::getKey)
-                .limit(3).collect(Collectors.toList());
+                .limit(3)
+                .collect(Collectors.toList());
     }
 }
