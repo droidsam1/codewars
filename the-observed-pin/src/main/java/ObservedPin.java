7179f3f9203a51b4ee3b37b9e72414bc527bba76
droidsam1 <droidsam@gmail.com>
2023-01-21 19:04:03 +0100

    refactor to keypadAdjacentMap

--- the-observed-pin/src/main/java/ObservedPin.java
+++ the-observed-pin/src/main/java/ObservedPin.java
@@ -7,28 +7,15 @@ public class ObservedPin {
     private static final Map<String, List<String>> keypadAdjacentMap = buildKeypadAdjacentMap();
 
     public static List<String> getPINs(String entered) {
-        if (entered.equals("0")) {
-            return keypadAdjacentMap.get(entered);
-        }
-
-        if (entered.equals("1")) {
-            return List.of("1", "2", "4");
-        }
-
-        if (entered.equals("7")) {
-            return List.of("7", "8", "4");
-        }
-
-        if (entered.equals("9")) {
-            return List.of("9", "8", "6");
-        }
-
-        return List.of(entered);
+        return keypadAdjacentMap.get(entered);
     }
 
     private static Map<String, List<String>> buildKeypadAdjacentMap() {
         Map<String, List<String>> keypadAdjacentMap = new HashMap<>();
         keypadAdjacentMap.put("0", List.of("0", "8"));
+        keypadAdjacentMap.put("1", List.of("1", "2", "4"));
+        keypadAdjacentMap.put("7", List.of("7", "8", "4"));
+        keypadAdjacentMap.put("9", List.of("9", "8", "6"));
         return keypadAdjacentMap;
     }
 }
