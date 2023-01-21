2f00a292f75a5af6058556012bdc972cb4ad8d78
droidsam1 <droidsam@gmail.com>
2023-01-21 18:51:34 +0100

    refactor to parameterizedTest

--- the-observed-pin/src/test/java/ObservedPinTest.java
+++ the-observed-pin/src/test/java/ObservedPinTest.java
@@ -1,11 +1,19 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ObservedPinTest {
 
+
+    public static Stream<Arguments> shouldReturnAdjacentDigitsWhenInputIsOneDigitOnly() {
+        return Stream.of(Arguments.of("1", List.of("1", "2", "4")));
+    }
 
     @Test
     void shouldReturnTheSameInputDigitWhenInputIsOneDigitOnly() {
@@ -16,10 +24,9 @@ class ObservedPinTest {
         assertTrue(digits.contains(input));
     }
 
-    @Test
-    void shouldReturnAdjacentDigitsWhenInputIsOneDigitOnly() {
-        var input = "1";
-        var expected = List.of("1", "2", "4");
+    @ParameterizedTest
+    @MethodSource
+    void shouldReturnAdjacentDigitsWhenInputIsOneDigitOnly(String input, List<String> expected) {
 
         var digits = ObservedPin.getPINs(input);
 
