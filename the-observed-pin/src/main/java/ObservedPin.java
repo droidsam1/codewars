e2878cb3eb5027d699e92bc50d556f6f08ece02f
droidsam1 <droidsam@gmail.com>
2023-01-21 19:10:42 +0100

    getPins resturns adjacent digits when input is 3

--- the-observed-pin/src/main/java/ObservedPin.java
+++ the-observed-pin/src/main/java/ObservedPin.java
@@ -15,6 +15,7 @@ public class ObservedPin {
         keypadAdjacentMap.put("0", List.of("0", "8"));
         keypadAdjacentMap.put("1", List.of("1", "2", "4"));
         keypadAdjacentMap.put("2", List.of("1", "2", "3", "5"));
+        keypadAdjacentMap.put("3", List.of("3", "2", "6"));
         keypadAdjacentMap.put("7", List.of("7", "8", "4"));
         keypadAdjacentMap.put("9", List.of("9", "8", "6"));
         return keypadAdjacentMap;
