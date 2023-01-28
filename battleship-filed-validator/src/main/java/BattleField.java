57a57a30ab83233312e85d288fc457c6ef519090
droidsam1 <droidsam@gmail.com>
2023-01-28 09:47:39 +0100

    first commit

---
+++ battleship-filed-validator/src/main/java/BattleField.java
@@ -0,0 +1,5 @@
+public class BattleField {
+    public static boolean fieldValidator(int[][] battleField) {
+        throw new RuntimeException("Not implemented");
+    }
+}
---
+++ battleship-filed-validator/src/test/java/SolutionTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SolutionTest {
+
+    private static final int[][] battleField = {{1, 0, 0, 0, 0, 1, 1, 0, 0, 0},
+            {1, 0, 1, 0, 0, 0, 0, 0, 1, 0},
+            {1, 0, 1, 0, 1, 1, 1, 0, 1, 0},
+            {1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+            {0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+            {0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
+
+    @Test
+    public void SampleTest() {
+        assertTrue(BattleField.fieldValidator(battleField));
+    }
+}
