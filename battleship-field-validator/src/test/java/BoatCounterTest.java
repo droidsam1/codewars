1e510f3e8fd5f57013dadc99241623e9ca277ee4
droidsam1 <droidsam@gmail.com>
2023-01-28 15:29:12 +0100

    refactor: strengthen the tests of boat counter

--- battleship-field-validator/src/test/java/BoatCounterTest.java
+++ battleship-field-validator/src/test/java/BoatCounterTest.java
@@ -1,4 +1,3 @@
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import utils.BattleFieldGenerator;
@@ -11,7 +10,7 @@ import static utils.BattleFieldGenerator.BOARD_SIZE;
 class BoatCounterTest {
 
     private static int[] runTenTimes() {
-        return IntStream.range(0, BOARD_SIZE).toArray();
+        return IntStream.rangeClosed(0, BOARD_SIZE).toArray();
     }
 
     @ParameterizedTest
@@ -24,10 +23,9 @@ class BoatCounterTest {
         assertEquals(expectedBattleship, numberOfBattleships);
     }
 
-    @Test
-    void shouldReturnTheNumberOfCruisers() {
-        var expectedCruisers = 1;
-
+    @ParameterizedTest
+    @MethodSource("runTenTimes")
+    void shouldReturnTheNumberOfCruisers(int expectedCruisers) {
         var battleField = BattleFieldGenerator.generateFieldWithNCruisers(expectedCruisers);
 
         var numberOfBattleships = BoatCounter.getNumberOfCruisers(battleField);
