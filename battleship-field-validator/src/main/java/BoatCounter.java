2a8592c29be1de9aa421e5d4ca0c73865f1b089f
droidsam1 <droidsam@gmail.com>
2023-01-28 16:05:17 +0100

    refactor: reduce duplicity in boat counter. Create boat types enum

--- battleship-field-validator/src/main/java/BoatCounter.java
+++ battleship-field-validator/src/main/java/BoatCounter.java
@@ -1,34 +1,35 @@
 import domain.BoatType;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class BoatCounter {
 
     static int getNumberOfBattleships(int[][] battleField) {
-
-        var occupiedCells = 0;
-        for (int i = 0; i < battleField.length; i++) {
-            for (int j = 0; j < battleField[i].length; j++) {
-                if (battleField[i][j] == 1) {
-                    occupiedCells++;
-                }
-            }
-
-        }
-
-        return occupiedCells / BoatType.BATTLESHIP.getSize();
+        return getNumberOf(battleField, BoatType.BATTLESHIP);
     }
 
     static int getNumberOfCruisers(int[][] battleField) {
+        return getNumberOf(battleField, BoatType.CRUISER);
+    }
 
-        var occupiedCells = 0;
+    static int getNumberOf(int[][] battleField, BoatType boatType) {
+
+        Map<BoatType, Integer> boats = new HashMap<>();
         for (int i = 0; i < battleField.length; i++) {
+            var occupiedCellsInThisRow = 0;
             for (int j = 0; j < battleField[i].length; j++) {
                 if (battleField[i][j] == 1) {
-                    occupiedCells++;
+                    occupiedCellsInThisRow++;
+                } else if (occupiedCellsInThisRow != 0) {
+                    var boat = BoatType.ofSize(occupiedCellsInThisRow);
+                    boats.put(boat, boats.getOrDefault(boat, 0) + 1);
+                    occupiedCellsInThisRow = 0;
                 }
             }
 
         }
 
-        return occupiedCells / BoatType.CRUISER.getSize();
+        return boats.getOrDefault(boatType, 0);
     }
 }
--- battleship-field-validator/src/main/java/domain/BoatType.java
+++ battleship-field-validator/src/main/java/domain/BoatType.java
@@ -1,5 +1,7 @@
 package domain;
 
+import java.util.Arrays;
+
 public enum BoatType {
 
     BATTLESHIP(4), CRUISER(3);
@@ -12,5 +14,9 @@ public enum BoatType {
 
     public int getSize() {
         return size;
+    }
+
+    public static BoatType ofSize(int boatSize) {
+        return Arrays.stream(BoatType.values()).filter(type -> type.getSize() == boatSize).findFirst().orElseThrow();
     }
 }
