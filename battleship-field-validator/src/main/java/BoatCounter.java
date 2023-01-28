d5af2a9b75f31390fdae0c4ce6c726e1611afd91
droidsam1 <droidsam@gmail.com>
2023-01-28 18:10:29 +0100

    refactor boat counter to extract methods

--- battleship-field-validator/src/main/java/BoatCounter.java
+++ battleship-field-validator/src/main/java/BoatCounter.java
@@ -6,21 +6,13 @@ import java.util.Map;
 public class BoatCounter {
 
     public static int getNumberOf(int[][] battleField, BoatType boatType) {
+        var ships = getShipsInRows(battleField);
+        ships.putAll(getShipsInCols(battleField));
+        return ships.getOrDefault(boatType, 0);
+    }
 
-        Map<BoatType, Integer> boats = new HashMap<>();
-        for (int i = 0; i < battleField.length; i++) {
-            var occupiedCellsInThisRow = 0;
-            for (int j = 0; j < battleField[i].length; j++) {
-                if (battleField[i][j] == 1) {
-                    occupiedCellsInThisRow++;
-                } else if (occupiedCellsInThisRow != 0) {
-                    var boat = BoatType.ofSize(occupiedCellsInThisRow);
-                    boats.put(boat, boats.getOrDefault(boat, 0) + 1);
-                    occupiedCellsInThisRow = 0;
-                }
-            }
-        }
-
+    private static Map<BoatType, Integer> getShipsInCols(int[][] battleField) {
+        Map<BoatType, Integer> ships = new HashMap<>();
         for (int i = 0; i < battleField.length; i++) {
             var occupiedCellsInThisCol = 0;
             for (int j = 0; j < battleField[i].length; j++) {
@@ -29,12 +21,29 @@ public class BoatCounter {
                 } else if (occupiedCellsInThisCol == 1) {
                     occupiedCellsInThisCol = 0;
                 } else if (occupiedCellsInThisCol > 1) {
-                    var boat = BoatType.ofSize(occupiedCellsInThisCol);
-                    boats.put(boat, boats.getOrDefault(boat, 0) + 1);
+                    var ship = BoatType.ofSize(occupiedCellsInThisCol);
+                    ships.put(ship, ships.getOrDefault(ship, 0) + 1);
                     occupiedCellsInThisCol = 0;
                 }
             }
         }
-        return boats.getOrDefault(boatType, 0);
+        return ships;
+    }
+
+    private static Map<BoatType, Integer> getShipsInRows(int[][] battleField) {
+        Map<BoatType, Integer> ships = new HashMap<>();
+        for (int i = 0; i < battleField.length; i++) {
+            var occupiedCellsInThisRow = 0;
+            for (int j = 0; j < battleField[i].length; j++) {
+                if (battleField[i][j] == 1) {
+                    occupiedCellsInThisRow++;
+                } else if (occupiedCellsInThisRow != 0) {
+                    var ship = BoatType.ofSize(occupiedCellsInThisRow);
+                    ships.put(ship, ships.getOrDefault(ship, 0) + 1);
+                    occupiedCellsInThisRow = 0;
+                }
+            }
+        }
+        return ships;
     }
 }
