e887234fba4e0341ec07048ab73d2cc0b8a8bf9e
droidsam1 <droidsam@gmail.com>
2023-02-25 10:22:43 +0100

    initial commit

---
+++ find-the-unknown-digit/src/main/java/runes/Runes.java
@@ -0,0 +1,18 @@
+package runes;
+
+public class Runes {
+
+    private Runes() {
+    }
+
+    public static int solveExpression(final String expression) {
+        int missingDigit = -1;
+
+        //Write code to determine the missing digit or unknown rune
+        //Expression will always be in the form
+        //(number)[operator](number)=(number)
+        //Unknown digit will not be the same as any other digits used in expression
+
+        return missingDigit;
+    }
+}
---
+++ find-the-unknown-digit/src/test/java/RunesTest.java
@@ -0,0 +1,19 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import runes.Runes;
+import org.junit.jupiter.api.Test;
+
+class RunesTest {
+
+    @Test void testSample() {
+        assertEquals(2, Runes.solveExpression("1+1=?"), "Answer for expression '1+1=?' ");
+        assertEquals(6, Runes.solveExpression("123*45?=5?088"), "Answer for expression '123*45?=5?088' ");
+        assertEquals(0, Runes.solveExpression("-5?*-1=5?"), "Answer for expression '-5?*-1=5?' ");
+        assertEquals(-1, Runes.solveExpression("19--45=5?"), "Answer for expression '19--45=5?' ");
+        assertEquals(5, Runes.solveExpression("??*??=302?"), "Answer for expression '??*??=302?' ");
+        assertEquals(2, Runes.solveExpression("?*11=??"), "Answer for expression '?*11=??' ");
+        assertEquals(2, Runes.solveExpression("??*1=??"), "Answer for expression '??*1=??' ");
+        assertEquals(-1, Runes.solveExpression("??+??=??"), "Answer for expression '??+??=??' ");
+    }
+
+}
