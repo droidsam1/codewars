7549f910b67de3f9b36f6204522124306f4d9f4d
droidsam1 <droidsam@gmail.com>
2023-03-04 14:57:59 +0100

    refactor: extract constant example sentence

--- decode-morse-code-advance/src/test/java/solution/part_two/MorseCodeDecoderTest.java
+++ decode-morse-code-advance/src/test/java/solution/part_two/MorseCodeDecoderTest.java
@@ -10,6 +10,8 @@ import org.junit.jupiter.api.Test;
 
 class MorseCodeDecoderTest {
 
+    private static final String BINARY_SENTENCE_ONE_BIT_PER_SYMBOL = "10101010001000111010111011100000001011101110111000101011100011101010001";
+
     @Test void shouldBeAbleToDecodeSimpleLetterAtSimpleRate() {
         assertThat(decodeMorse(decodeBits("1")), is("E"));
         assertThat(decodeMorse(decodeBits("10")), is("A"));
@@ -17,20 +19,14 @@ class MorseCodeDecoderTest {
     }
 
     @Test void shouldBeAbleToDecodeSentenceAtSimpleRate() {
-        assertThat(decodeBits("10101010001000111010111011100000001011101110111000101011100011101010001"),
-                   is(".... . -.--   .--- ..- -.. .")
-        );
-
-        assertThat(
-                decodeMorse(decodeBits("10101010001000111010111011100000001011101110111000101011100011101010001")),
-                is("HEY JUDE")
-        );
+        assertThat(decodeBits(BINARY_SENTENCE_ONE_BIT_PER_SYMBOL), is(".... . -.--   .--- ..- -.. ."));
+        assertThat(decodeMorse(decodeBits(BINARY_SENTENCE_ONE_BIT_PER_SYMBOL)), is("HEY JUDE"));
     }
 
-    @Disabled @Test void testExampleFromDescription() {
+    @Disabled("Disable while developing with TDD") @Test void testExampleFromDescription() {
         assertThat(
                 decodeMorse(decodeBits(
-                        "1100110011001100 0000 11 0000 00111111001100111111001111110000000000000011001111110011111100111111000000110011001111110000001111110011001100000011")),
+                        "1100110011001100000011000000111111001100111111001111110000000000000011001111110011111100111111000000110011001111110000001111110011001100000011")),
                 is("HEY JUDE")
         );
     }
