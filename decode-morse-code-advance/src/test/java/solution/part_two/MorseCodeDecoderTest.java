b407612db021a46ecd2ab7878325c7f5c68d8165
droidsam1 <droidsam@gmail.com>
2023-03-04 11:03:25 +0100

    add: decoding sentences bits at sampling rate of 1 sample per symbol

--- decode-morse-code-advance/src/test/java/solution/part_two/MorseCodeDecoderTest.java
+++ decode-morse-code-advance/src/test/java/solution/part_two/MorseCodeDecoderTest.java
@@ -2,21 +2,27 @@ package solution.part_two;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static solution.part_two.MorseCodeDecoder.decodeBits;
+import static solution.part_two.MorseCodeDecoder.decodeMorse;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 class MorseCodeDecoderTest {
 
-    @Test void shouldBeAbleToDecodeSimpleLetter() {
-        assertThat(MorseCodeDecoder.decodeMorse(MorseCodeDecoder.decodeBits("1")), is("E"));
-        assertThat(MorseCodeDecoder.decodeMorse(MorseCodeDecoder.decodeBits("10")), is("A"));
-        assertThat(MorseCodeDecoder.decodeMorse(MorseCodeDecoder.decodeBits("111")), is("S"));
+    @Test void shouldBeAbleToDecodeSimpleLetterAtSimpleRate() {
+        assertThat(decodeMorse(decodeBits("1")), is("E"));
+        assertThat(decodeMorse(decodeBits("10")), is("A"));
+        assertThat(decodeMorse(decodeBits("111")), is("S"));
+    }
+
+    @Test void shouldBeAbleToDecodeSentenceAtSimpleRate() {
+        assertThat(decodeBits("1111 1 0100   1000 110 011 1"), is(".... . -.--   .--- ..- -.. ."));
     }
 
     @Disabled @Test void testExampleFromDescription() {
         assertThat(
-                MorseCodeDecoder.decodeMorse(MorseCodeDecoder.decodeBits(
+                decodeMorse(decodeBits(
                         "1100110011001100000011000000111111001100111111001111110000000000000011001111110011111100111111000000110011001111110000001111110011001100000011")),
                 is("HEY JUDE")
         );
