e77be7bed0c7ab00e64dc1b2a06e7fa5c0bf240c
droidsam1 <droidsam@gmail.com>
2023-03-04 14:09:31 +0100

    add: input is a binary string, does not contains spaces

--- decode-morse-code-advance/src/main/java/solution/part_two/MorseCodeDecoder.java
+++ decode-morse-code-advance/src/main/java/solution/part_two/MorseCodeDecoder.java
@@ -1,13 +1,16 @@
 package solution.part_two;
 
-
 public class MorseCodeDecoder {
 
     private MorseCodeDecoder() {
     }
 
     public static String decodeBits(String bits) {
-        return bits.replace("1", ".").replace("0", "-");
+        return bits.replace("0000000", "   ")
+                   .replace("000", " ")
+                   .replace("111", "-")
+                   .replace("1", ".")
+                   .replace("0", "");
     }
 
     public static String decodeMorse(String morseCode) {
--- decode-morse-code-advance/src/test/java/solution/part_two/MorseCodeDecoderTest.java
+++ decode-morse-code-advance/src/test/java/solution/part_two/MorseCodeDecoderTest.java
@@ -17,14 +17,20 @@ class MorseCodeDecoderTest {
     }
 
     @Test void shouldBeAbleToDecodeSentenceAtSimpleRate() {
-        assertThat(decodeBits("1111 1 0100   1000 110 011 1"), is(".... . -.--   .--- ..- -.. ."));
-        assertThat(decodeMorse(decodeBits("1111 1 0100   1000 110 011 1")), is("HEY JUDE"));
+        assertThat(decodeBits("10101010001000111010111011100000001011101110111000101011100011101010001"),
+                   is(".... . -.--   .--- ..- -.. .")
+        );
+
+        assertThat(
+                decodeMorse(decodeBits("10101010001000111010111011100000001011101110111000101011100011101010001")),
+                is("HEY JUDE")
+        );
     }
 
     @Disabled @Test void testExampleFromDescription() {
         assertThat(
                 decodeMorse(decodeBits(
-                        "1100110011001100000011000000111111001100111111001111110000000000000011001111110011111100111111000000110011001111110000001111110011001100000011")),
+                        "1100110011001100 0000 11 0000 00111111001100111111001111110000000000000011001111110011111100111111000000110011001111110000001111110011001100000011")),
                 is("HEY JUDE")
         );
     }
