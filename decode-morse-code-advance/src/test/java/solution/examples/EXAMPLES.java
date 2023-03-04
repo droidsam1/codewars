99c9022ef9d5a7fdaeb930555294c8af371fc74b
droidsam1 <droidsam@gmail.com>
2023-03-04 15:12:52 +0100

    refactor: reduce duplicity

--- decode-morse-code-advance/src/test/java/solution/examples/EXAMPLES.java
+++ decode-morse-code-advance/src/test/java/solution/examples/EXAMPLES.java
@@ -21,11 +21,24 @@ public enum EXAMPLES {
         return binaryString;
     }
 
+    public String getBinaryString(int withSamplingRateOf) {
+        return withNewSampling(binaryString, withSamplingRateOf);
+    }
+
     public String getMorseString() {
         return morseString;
     }
 
     public String getEnglishString() {
         return englishString;
+    }
+
+    private String withNewSampling(String input, int samplesPerSymbol) {
+        var array = input.toCharArray();
+        var result = new StringBuilder();
+        for (char c : array) {
+            result.append(String.valueOf(c).repeat(samplesPerSymbol));
+        }
+        return result.toString();
     }
 }
--- decode-morse-code-advance/src/test/java/solution/part_two/MorseDecoderSampleRateTest.java
+++ decode-morse-code-advance/src/test/java/solution/part_two/MorseDecoderSampleRateTest.java
@@ -2,30 +2,20 @@ package solution.part_two;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static solution.examples.EXAMPLES.HEY_JUDE_EXAMPLE;
 import static solution.part_two.MorseDecoderSampleRate.getSamplingRate;
 
 import org.junit.jupiter.api.Test;
 
 class MorseDecoderSampleRateTest {
 
-    private static final String EXAMPLE_SENTENCE = "111110010000010000110001101";
-
     @Test void shouldGetSamplingRate() {
         assertThat(getSamplingRate("1"), is(1));
         assertThat(getSamplingRate("10"), is(1));
         assertThat(getSamplingRate("111"), is(1));
         assertThat(getSamplingRate("1100"), is(2));
-        assertThat(getSamplingRate(EXAMPLE_SENTENCE), is(1));
-        assertThat(getSamplingRate(withNewSampling(EXAMPLE_SENTENCE, 2)), is(2));
-        assertThat(getSamplingRate(withNewSampling(EXAMPLE_SENTENCE, 4)), is(4));
-    }
-
-    private String withNewSampling(String input, int samplesPerSymbol) {
-        var array = input.toCharArray();
-        var result = new StringBuilder();
-        for (char c : array) {
-            result.append(String.valueOf(c).repeat(samplesPerSymbol));
-        }
-        return result.toString();
+        assertThat(getSamplingRate(HEY_JUDE_EXAMPLE.getBinaryString()), is(1));
+        assertThat(getSamplingRate(HEY_JUDE_EXAMPLE.getBinaryString(2)), is(2));
+        assertThat(getSamplingRate(HEY_JUDE_EXAMPLE.getBinaryString(4)), is(4));
     }
 }
