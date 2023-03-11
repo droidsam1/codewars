aba752bd16f5567e5c3f65bb0576002c5b3325bf
droidsam1 <droidsam@gmail.com>
2023-03-11 09:36:02 +0100

    initial code

---
+++ human-readable-duration-format/src/main/java/time_formatter/TimeFormatter.java
@@ -0,0 +1,9 @@
+package time_formatter;
+
+public class TimeFormatter {
+
+    public static String formatDuration(int seconds) {
+        // your code goes here
+        return null;
+    }
+}
---
+++ human-readable-duration-format/src/test/java/time_formatter/TimeFormatterTest.java
@@ -0,0 +1,18 @@
+package time_formatter;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+class TimeFormatterTest {
+
+    @Test @Disabled("while developing with TDD") void exampleTests() {
+        assertEquals("1 second", TimeFormatter.formatDuration(1));
+        assertEquals("1 minute and 2 seconds", TimeFormatter.formatDuration(62));
+        assertEquals("2 minutes", TimeFormatter.formatDuration(120));
+        assertEquals("1 hour", TimeFormatter.formatDuration(3600));
+        assertEquals("1 hour, 1 minute and 2 seconds", TimeFormatter.formatDuration(3662));
+    }
+
+}
