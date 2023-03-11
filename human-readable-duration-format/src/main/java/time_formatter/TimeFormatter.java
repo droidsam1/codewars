fb4010dbedd581e6a1eec8f283c7a46a5b19739e
droidsam1 <droidsam@gmail.com>
2023-03-11 11:07:38 +0100

    refactor: create an enum for time units

--- human-readable-duration-format/src/main/java/time_formatter/TimeFormatter.java
+++ human-readable-duration-format/src/main/java/time_formatter/TimeFormatter.java
@@ -43,22 +43,22 @@ public class TimeFormatter {
     }
 
     private static String getDays(int seconds) {
-        return format(seconds / SECONDS_IN_ONE_DAY, DAY);
+        return format(seconds / TIME_UNITS.DAY.seconds, TIME_UNITS.DAY.name);
     }
 
     private static String getHours(int seconds) {
-        return format(seconds / SECONDS_IN_AN_HOUR, HOUR);
+        return format(seconds / TIME_UNITS.HOUR.seconds, TIME_UNITS.HOUR.name);
     }
 
     private static String getMinutes(int seconds) {
-        return format(seconds / SECONDS_IN_A_MINUTE, MINUTE);
+        return format(seconds / TIME_UNITS.MINUTE.seconds, TIME_UNITS.MINUTE.name);
     }
 
     private static String getSeconds(int seconds) {
-        return format(seconds % SECONDS_IN_A_MINUTE, SECOND);
+        return format(seconds / TIME_UNITS.SECONDS.seconds, TIME_UNITS.SECONDS.name);
     }
 
-    private static String format(int seconds, String unitOfTime) {
+    private static String format(long seconds, String unitOfTime) {
         if (seconds == 0) {
             return "";
         }
@@ -67,5 +67,18 @@ public class TimeFormatter {
             return String.format(TIME_UNIT_FORMAT, seconds, unitOfTime);
         }
         return String.format(TIME_UNIT_FORMAT, seconds, (unitOfTime + "s"));
+    }
+
+    enum TIME_UNITS {
+        DAY("day", 86400), HOUR("hour", 3600), MINUTE("minute", 60), SECONDS("second", 1);
+
+        private final String name;
+        private final long seconds;
+
+        TIME_UNITS(String name, int seconds) {
+
+            this.name = name;
+            this.seconds = seconds;
+        }
     }
 }
