d7ad3768d595fe9e57fc5f16b55b26c97e4ecf10
droidsam1 <droidsam@gmail.com>
2023-03-11 10:14:02 +0100

    refactor: extract common method to format time units

--- human-readable-duration-format/src/main/java/time_formatter/TimeFormatter.java
+++ human-readable-duration-format/src/main/java/time_formatter/TimeFormatter.java
@@ -5,35 +5,34 @@ public class TimeFormatter {
     private static final String MINUTE = "minute";
     private static final String SECOND = "second";
     private static final int SECONDS_IN_A_MINUTE = 60;
+    private static final String TIME_UNIT_FORMAT = "%s %s";
 
     private TimeFormatter() {
     }
 
     public static String formatDuration(int seconds) {
         if (seconds >= SECONDS_IN_A_MINUTE) {
-            return String.format("%s %s", getMinutes(seconds), getSeconds(seconds % SECONDS_IN_A_MINUTE)).trim();
+            return String.format(TIME_UNIT_FORMAT, getMinutes(seconds), getSeconds(seconds)).trim();
         }
         return getSeconds(seconds);
     }
 
     private static String getMinutes(int seconds) {
-        if (seconds / SECONDS_IN_A_MINUTE == 1) {
-            return String.format("%s " + MINUTE, seconds / SECONDS_IN_A_MINUTE);
-        }
-        if (seconds / SECONDS_IN_A_MINUTE > 1) {
-            return String.format("%s " + MINUTE + "s", seconds / SECONDS_IN_A_MINUTE);
-        }
-        return "";
+        return format(seconds / SECONDS_IN_A_MINUTE, MINUTE);
     }
 
     private static String getSeconds(int seconds) {
+        return format(seconds % SECONDS_IN_A_MINUTE, SECOND);
+    }
+
+    private static String format(int seconds, String unitOfTime) {
         if (seconds == 0) {
             return "";
         }
 
         if (seconds == 1) {
-            return String.format("%s " + SECOND, seconds);
+            return String.format(TIME_UNIT_FORMAT, seconds, unitOfTime);
         }
-        return String.format("%s " + SECOND + "s", seconds);
+        return String.format(TIME_UNIT_FORMAT, seconds, (unitOfTime + "s"));
     }
 }
