e13a06c8e97bb1ffd00b87b54371bd072899d9b6
droidsam1 <droidsam@gmail.com>
2023-03-25 09:24:43 +0100

    refactor: extract constants

--- hard-sudoku-solver/src/main/java/sudoku/SudokuSolver.java
+++ hard-sudoku-solver/src/main/java/sudoku/SudokuSolver.java
@@ -2,6 +2,8 @@ package sudoku;
 
 public class SudokuSolver {
 
+    private static final String INVALID_GRID_SIZE = "Invalid grid size";
+    private static final String INVALID_GRID_VALUE = "Invalid grid value";
     private final int[][] grid;
 
     public SudokuSolver(int[][] grid) {
@@ -15,15 +17,15 @@ public class SudokuSolver {
 
     private void validateGridIs9x9() {
         if (grid == null || grid.length != 9) {
-            throw new IllegalArgumentException("Invalid grid size");
+            throw new IllegalArgumentException(INVALID_GRID_SIZE);
         }
         for (var row : grid) {
             if (row.length != 9) {
-                throw new IllegalArgumentException("Invalid grid size");
+                throw new IllegalArgumentException(INVALID_GRID_SIZE);
             }
             for (var cell : row) {
                 if (cell < 0 || cell > 9) {
-                    throw new IllegalArgumentException("Invalid grid size");
+                    throw new IllegalArgumentException(INVALID_GRID_VALUE);
                 }
             }
         }
