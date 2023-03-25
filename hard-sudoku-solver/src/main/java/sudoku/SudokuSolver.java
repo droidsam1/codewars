28ac9aefb0d5cc75bd44552a65ae1ffc7fa19330
droidsam1 <droidsam@gmail.com>
2023-03-25 09:02:44 +0100

    add initial commit

---
+++ hard-sudoku-solver/src/main/java/sudoku/SudokuSolver.java
@@ -0,0 +1,13 @@
+package sudoku;
+
+public class SudokuSolver {
+
+    public SudokuSolver(int[][] grid) {
+        // Your code here!
+    }
+
+    public int[][] solve() {
+        // Your code here too!
+        return null;
+    }
+}
