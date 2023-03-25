4a8f502de12f382468b3d45892b283f89124fd55
droidsam1 <droidsam@gmail.com>
2023-03-25 19:21:26 +0100

    refactor: to symmetric difference

--- hard-sudoku-solver/src/main/java/sudoku/SudokuHelper.java
+++ hard-sudoku-solver/src/main/java/sudoku/SudokuHelper.java
@@ -5,6 +5,7 @@ import static java.util.stream.Collectors.toSet;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.function.Predicate;
 import java.util.stream.IntStream;
 
 public class SudokuHelper {
@@ -16,16 +17,25 @@ public class SudokuHelper {
     }
 
     public int[] candidatesInRowColumn(final int row, final int col) {
+        return toArray(findCandidatesBasedOnRowAndColumn(row, col));
+    }
+
+    private Set<Integer> findCandidatesBasedOnRowAndColumn(final int row, final int col) {
         var presentNumbersRow = new HashSet<Integer>();
         for (int i = 0; i < grid.length; i++) {
             presentNumbersRow.add(grid[row][i]);
             presentNumbersRow.add(grid[i][col]);
         }
 
-        return toArray(difference(IntStream.rangeClosed(1, 9).boxed().collect(toSet()), presentNumbersRow));
+        return difference(IntStream.rangeClosed(0, 9).boxed().collect(toSet()), presentNumbersRow);
     }
 
+
     public int[] findCandidatesForCellInItsOwnSubgrid(int row, int col) {
+        return toArray(findCandidatesInSubGrid(row, col));
+    }
+
+    private Set<Integer> findCandidatesInSubGrid(int row, int col) {
         var alreadyPresentNumbers = new HashSet<Integer>();
 
         for (int i = (row / 3) * 3; i < (row / 3) * 3 + 3; i++) {
@@ -34,7 +44,7 @@ public class SudokuHelper {
             }
         }
 
-        return toArray(difference(IntStream.rangeClosed(0, 9).boxed().collect(toSet()), alreadyPresentNumbers));
+        return difference(IntStream.rangeClosed(0, 9).boxed().collect(toSet()), alreadyPresentNumbers);
     }
 
     private int[] toArray(Collection<Integer> collection) {
@@ -43,7 +53,7 @@ public class SudokuHelper {
 
     private <T> Set<T> difference(final Set<T> setOne, final Set<T> setTwo) {
         Set<T> result = new HashSet<>(setOne);
-        result.removeAll(setTwo);
+        setTwo.stream().filter(Predicate.not(result::add)).forEach(result::remove);
         return result;
     }
 
