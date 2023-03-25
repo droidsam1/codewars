36b7d9c633200ecb43868476ce77bb377ce622ed
droidsam1 <droidsam@gmail.com>
2023-03-25 09:14:51 +0100

    refactor: extract validations to a method

--- hard-sudoku-solver/src/main/java/sudoku/SudokuSolver.java
+++ hard-sudoku-solver/src/main/java/sudoku/SudokuSolver.java
@@ -9,9 +9,19 @@ public class SudokuSolver {
     }
 
     public int[][] solve() {
-        if (grid == null || grid.length == 0 || grid.length != 9 && grid[0].length != 9) {
-            throw new IllegalArgumentException("Invalid grid size");
-        }
+        validateGridIs9x9();
         return null;
     }
+
+    private void validateGridIs9x9() {
+        if (grid == null || grid.length != 9) {
+            throw new IllegalArgumentException("Invalid grid size");
+        }
+        for (var row : grid) {
+            if (row.length != 9) {
+                throw new IllegalArgumentException("Invalid grid size");
+            }
+        }
+    }
+
 }
