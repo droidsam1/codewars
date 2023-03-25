0b40eee6c311820e23970099757b207332603788
droidsam1 <droidsam@gmail.com>
2023-03-25 10:46:28 +0100

    refactor: to parameterized test

--- hard-sudoku-solver/src/test/java/sudoku/SudokuHelperTest.java
+++ hard-sudoku-solver/src/test/java/sudoku/SudokuHelperTest.java
@@ -2,21 +2,30 @@ package sudoku;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class SudokuHelperTest {
 
-    @Test void shouldFindCandidatesForMissingCellInRowAndCol() {
-        var onlyOneMissingCellInput = new int[][]{
-                {0, 3, 5},//
-                {6, 8, 2}, //
-                {1, 9, 7},//
-        };
-        var expectedCandidatesForMissingCell = new int[]{2, 4, 7, 8, 9};
+    public static Stream<Arguments> candidatesPerRowAndCol() {
+        return Stream.of(Arguments.of(new int[][]{
+                                 {0, 3, 5},//
+                                 {6, 8, 2}, //
+                                 {1, 9, 7},//
+                         }, new int[]{2, 4, 7, 8, 9})
 
-        var sudokuSolver = new SudokuHelper(onlyOneMissingCellInput);
+        );
 
-        assertArrayEquals(expectedCandidatesForMissingCell, sudokuSolver.candidatesInRowColumn(0));
+    }
+
+    @ParameterizedTest @MethodSource("candidatesPerRowAndCol")
+    void shouldFindCandidatesForMissingCellInRowAndCol(int[][] grid, int[] candidates) {
+        var sudokuSolver = new SudokuHelper(grid);
+
+        assertArrayEquals(candidates, sudokuSolver.candidatesInRowColumn(0));
     }
 
     @Test void shouldFindCandidatesForMissingCellInTheGrid() {
